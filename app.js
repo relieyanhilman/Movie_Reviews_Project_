e2d85eb9db88c26f8627dcf8ec496b3711f5202b
relieyanhilman <relieyanhilman93@gmail.com>
2021-06-09 10:56:35 +0800

    sekarang bisa liat semua review

--- app.js
+++ app.js
@@ -6,10 +6,21 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
-app.get('/', (req, res) => {
-    res.render('home', {msg: 'Handlebars are Cool!'});
-})
+// app.get('/', (req, res) => {
+//     res.render('home', {msg: 'Handlebars are Cool!'});
+// })
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
+})
+
+
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II"},
+    { title: "Awesome Movie", movieTitle: "Titanic"}
+]
+
+//Index
+app.get('/', (req, res) => {
+    res.render('reviews-index', { reviews: reviews });
 })

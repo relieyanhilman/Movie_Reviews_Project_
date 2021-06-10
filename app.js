acb3b92d316ba5e5c68b76dc3cfa4508efce28fb
relieyanhilman <relieyanhilman93@gmail.com>
2021-06-10 09:33:25 +0800

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/acme', {useNewUrlParser:true, useUnifiedTopology: true});
+
 const app = express();
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+});
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -15,12 +23,18 @@ app.listen(3000, () => {
 })
 
 
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman II"},
-    { title: "Awesome Movie", movieTitle: "Titanic"}
-]
+// let reviews = [
+//     { title: "Great Review", movieTitle: "Batman II"},
+//     { title: "Awesome Movie", movieTitle: "Dragon Ball SUpper"}
+// ]
 
 //Index
 app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
+    Review.find()
+    .then(reviews => {
+        res.render('reviews-index', { reviews: reviews});
+    })
+    .catch(err => {
+        console.log(err);
+    })
 })

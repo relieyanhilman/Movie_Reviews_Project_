60768a7757250b8f34389e444b027694f7f15054
relieyanhilman <relieyanhilman93@gmail.com>
2021-06-10 17:15:01 +0800

    Users can see single reviews

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost:27017/acme', {useNewUrlParser:true, useUnifiedTopology: true});
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true}));
 
 const Review = mongoose.model('Review', {
     title: String,
     movieTitle: String,
-    description: String
+    description: String,
+    Rating: Number
 });
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 
 // app.get('/', (req, res) => {
 //     res.render('home', {msg: 'Handlebars are Cool!'});
@@ -34,7 +38,7 @@ app.listen(3000, () => {
 
 //Index
 app.get('/', (req, res) => {
-    Review.find()
+    Review.find().lean()
     .then(reviews => {
         res.render('reviews-index', { reviews: reviews});
         // console.log('kalo ada data, bakal ditampilin');
@@ -44,6 +48,7 @@ app.get('/', (req, res) => {
     })
 })
 
+// Menuju ke FORM
 app.get('/reviews/new', (req, res) => {
     res.render('reviews-new', {});
 })
@@ -59,8 +64,20 @@ app.post('/reviews', (req, res) => {
     Review.create(req.body)
         .then((review) => {
             console.log(review);
+            res.redirect(`/reviews/${review._id}`);
         })
         .catch((err) => {
             console.log(err.message);
         })
+})
+
+//SHOW dari databases berdasarkan id
+app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).lean()
+    .then((review) => {
+        res.render('reviews-show', {review: review })
+    })
+    .catch((err) => {
+        console.log(err.message);
+    })
 })
